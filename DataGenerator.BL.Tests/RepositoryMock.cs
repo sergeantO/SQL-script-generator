4f82ea04ef9f9ce2b38148b68621b568235c4ef7
SergeantO <morgueheadman@gmail.com>
2019-10-07 23:49:02 +0300

    scriptGenerator completed

---
+++ DataGenerator.BL.Tests/RepositoryMock.cs
@@ -0,0 +1,42 @@
+﻿using DataGenerator.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGenerator.BL.Tests
+{
+    public class RepositoryMock : IRepository
+    {
+        public string GetRandomEmailDomain()
+        {
+            return "gmail.com";
+        }
+
+        public string GetRandomName()
+        {
+            return "Иван";
+        }
+
+        public string GetRandomPatronymic()
+        {
+            return "Иванович";
+        }
+
+        public string GetRandomSurname()
+        {
+            return "Иванов";
+        }
+
+        public string GetRandomUniqLogin()
+        {
+            return "Ivan";
+        }
+
+        public void Init()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
